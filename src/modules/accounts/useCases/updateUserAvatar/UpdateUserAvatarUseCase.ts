44038bc0ec3dbd12deec4e1157dceaf922d2f0b5
Gustavo Marques <gustavomarquesmm@gmail.com>
2022-03-09 13:23:08 -0300

    Pasta Utils para usar funções úteis no sistema

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from "tsyringe"
+import {deleteFile} from "../../../../utils/file"
 import { IUsersRepository } from "../../repositories/IUsersRepository"
 
 interface IRequest {
@@ -12,9 +13,13 @@ class UpdateUserAvatarUseCase{
         @inject("UsersRepository")
         private usersRepository: IUsersRepository
     ){}
+
     async execute({user_id, avatar_file}: IRequest): Promise<void>{
          const user = await this.usersRepository.findById(user_id)
 
+        if (user.avatar){
+            await deleteFile(`./tmp/avatar/${user.avatar}`)
+        }
          user.avatar = avatar_file
 
          await this.usersRepository.create(user)
