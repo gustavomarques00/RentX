7ad2a9e95625c10c6472171331b51c1e6b50897c
Gustavo Marques <gustavomarquesmm@gmail.com>
2022-03-11 16:58:18 -0300

    Migration da tabela "cars"

--- src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -2,8 +2,8 @@ import { Category } from "@modules/cars/infra/typeorm/entities/Category"
 import { ICategoriesRepository, ICreateCategoryDTO } from "../ICategoriesRepository"
 
 class CategoriesRepositoryInMemory implements ICategoriesRepository {
-
     categories: Category[] = []
+    
     async findByName(name: string): Promise<Category> {
         const category = this.categories.find((category) => category.name === name)
         return category
