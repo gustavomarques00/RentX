56d324ea446338e1b297ee88cd108f255008b457
Gustavo Marques <gustavomarquesmm@gmail.com>
2022-03-14 16:14:40 -0300

    Criação do UseCase "createCarSpecification"

--- src/modules/cars/repositories/ISpecificationRepository.ts
+++ src/modules/cars/repositories/ISpecificationRepository.ts
@@ -1,14 +1,14 @@
-import { Specification } from "../infra/typeorm/entities/Specification"
+import { Specification } from "@modules/cars/infra/typeorm/entities/Specification"
 
 interface ICreateSpecificationDTO{
     name: string,
     description: string
 }
 
-
 interface ISpecificationRepository{
-    create({name,description}: ICreateSpecificationDTO): Promise<void>
+    create({name,description}: ICreateSpecificationDTO): Promise<Specification>
     findByName(name:string): Promise<Specification>
+    findByIds(ids: string[]):Promise<Specification[]>
 }
 
 export {ISpecificationRepository,ICreateSpecificationDTO}
---
+++ src/shared/infra/typeorm/migrations/1647284985007-CreateCarImages.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateCarImages1647284985007 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
